4a03382b82a13b7a74d89323ab7ad134bc49c36b
recurze <rahulv.9942@gmail.com>
2024-10-14 00:42:37 +0800

    [iv] Update implied volatility computation
    * Add S, r, q and T func parameters
    * Use standard notation in place of verbose variable names
    
    Note: this will remain unused for worth retaining the file for display

--- implied_volatility.py
+++ implied_volatility.py
@@ -1,19 +1,17 @@
 import math
+
+from options import Option
+from options import PriceType
 from scipy.optimize import bisect
-from options import (
-    Option,
-    PriceType,
-)
-from typing import (
-    Optional,
-    Callable,
-)
+from typing import Callable
 
 
 def approximate_implied_volatility(option: Option,
                                    value: PriceType,
-                                   risk_free_rate: float,
-                                   time_to_maturity_in_years: float) -> float:
+                                   S: PriceType,
+                                   r: float,
+                                   q: float | None,
+                                   T: float) -> float:
     """
     Bharadia, M. A. J., N. Christofides, and G. R. Salkin.
     "Computing the Black-Scholes implied volatility: Generalization of a simple formula."
@@ -25,46 +23,49 @@ def approximate_implied_volatility(option: Option,
     """
 
     # C = SQ*N(d + x) - KD*N(d - x)
-    # x = 1/2 * sigma * sqrt(tau), d = (ln(F) - ln(K))/(sigma * sqrt(tau)) where F = SQ/D
+    # x = 1/2 * sigma * sqrt(T), d = (ln(F) - ln(K))/(sigma * sqrt(T)) where F = SQ/D
     #
     # N(d + x) - N(d - x) = 2xN'(d)  from Taylor's approximation
     # C = SQ[N(d + x) - N(d - x)] + (2delta)N(d - x) where delta = (SQ - KD)/2
     # C = 2xSQN'(d) + 2delta*N(d - x)
     #
     # When d is close to 0,
-    # C = sigma * sqrt(tau) * SQ / sqrt(2pi) + 2 * delta * (1/2 - sigma*sqrt(tau)/sqrt(2pi))
-    # sigma = (C - delta)/(SQ - delta) * sqrt(2pi / tau)
+    # C = sigma * sqrt(T) * SQ / sqrt(2pi) + 2 * delta * (1/2 - sigma*sqrt(T)/sqrt(2pi))
+    # sigma = (C - delta)/(SQ - delta) * sqrt(2pi / T)
 
-    tau = time_to_maturity_in_years
+    K = option.K
+    q = q or 0
 
-    S = option.underlying.spot_price
-    K = option.strike_price
-
-    r = risk_free_rate
-    q = option.dividend_yield
-    D = math.exp(-r*tau)
-    Q = math.exp(-q*tau)
+    D = math.exp(-r*T)
+    Q = math.exp(-q*T)
 
     delta = (S*Q - K*D)/2
 
     if option.is_call():
-        return math.sqrt(2 * math.pi / tau) * (value - delta) / (S - delta)
+        return math.sqrt(2 * math.pi / T) * (value - delta) / (S - delta)
 
-    return math.sqrt(2 * math.pi / tau) * (value + delta) / (S - delta)
+    if option.is_put():
+        return math.sqrt(2 * math.pi / T) * (value + delta) / (S - delta)
+
+    raise NotImplementedError()
 
 
 def compute_implied_volatility(option: Option,
                                value: PriceType,
+                               S: PriceType,
+                               r: float,
+                               q: float | None,
+                               T: float,
                                pricer: Callable,
                                **kwargs) -> float:
     # https://en.wikipedia.org/wiki/Newton%27s_method#Code
     def newtons(initial_guess_vol: float,
                 tol: float = 1e-6,
                 eps: float = 1e-6,
-                maxiter: int = 100) -> Optional[float]:
+                maxiter: int = 100) -> float | None:
         vol = initial_guess_vol
         for i in range(maxiter):
-            computed_value, greeks = pricer(option=option, volatility=vol, **kwargs)
+            computed_value, greeks = pricer(option, S, r, q, T, vol, **kwargs)
 
             vega = greeks["vega"]
             if abs(vega) < eps:
@@ -83,15 +84,10 @@ def compute_implied_volatility(option: Option,
                       tol: float = 1e-6,
                       maxiter: int = 100) -> float:
         def f(vol):
-            computed_value, greeks = pricer(option=option, volatility=vol, **kwargs)
+            computed_value, greeks = pricer(option, S, r, q, T, vol, **kwargs)
             return value - computed_value
 
         return bisect(f, min_vol, max_vol, xtol=tol, maxiter=maxiter)
 
-    initial_guess = approximate_implied_volatility(
-        option,
-        value,
-        kwargs["risk_free_rate"],
-        kwargs["time_to_maturity_in_years"]
-    )
+    initial_guess = approximate_implied_volatility(option, value, S, r, q, T)
     return newtons(initial_guess) or binary_search() or initial_guess
